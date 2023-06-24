e39e5aa48ad5d47b29d6b1a5f58854f38731e324
1C3Zup <nuttapob33@gmail.com>
2023-06-24 11:15:07 +0700

    "JavaScript"

--- App.js
+++ App.js
@@ -15,6 +15,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:
+    
+    'center',
   },
 });

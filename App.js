1fd0484accb072150d5829843237609685cd2526
1C3Zup <nuttapob33@gmail.com>
2023-08-26 11:19:53 +0700

    First Commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
-
+import New from './components/New'
 
 const App = () => {
   return (
     <View style={styles.container}>
         <FlatList_HeaderFooter/>
+        <New/>
     </View>
   )
 }

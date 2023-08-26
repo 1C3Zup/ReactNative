a3aa0a064f2ea3cee9dbbe5fbff43e3cf654e034
1C3Zup <nuttapob33@gmail.com>
2023-08-26 11:49:22 +0700

    Second Commit

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
+import FlatListAPI from './components/FlatListAPI'
 import New from './components/New'
+import ProductScreen from './components/ProductScreen'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-        <FlatList_HeaderFooter/>
-        <New/>
+    <View>
+      <New/>
+      <ProductScreen/>
     </View>
   )
 }
-
-export default App
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,   
-    justifyContent:'left',    
-    alignItems:'left'   
- }
-})
